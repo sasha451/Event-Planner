4253ca61a1f0749da336c27e167b4ab96c655fb0
sasha451 <shulga.sasha.new@gmail.com>
2019-11-20 20:21:56 +0300

    feat: add ITaskListUpdateListener

---
+++ Application/TaskPlanner/app/src/main/java/com/shulga/taskplanner/interfaces/ITaskListUpdateListener.java
@@ -0,0 +1,8 @@
+package com.shulga.taskplanner.interfaces;
+
+/**
+ * Listener to be called whenever task list is udpated by add, update or delete.
+ */
+public interface ITaskListUpdateListener{
+    void onTaskListUpdate();
+}
