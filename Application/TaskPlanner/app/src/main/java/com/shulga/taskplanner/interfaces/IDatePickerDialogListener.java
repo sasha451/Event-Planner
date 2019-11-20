97666a678098eba6d111741f952d8bce417033f0
sasha451 <shulga.sasha.new@gmail.com>
2019-11-20 20:19:01 +0300

    feat: add IDatePickerDialogListener

---
+++ Application/TaskPlanner/app/src/main/java/com/shulga/taskplanner/interfaces/IDatePickerDialogListener.java
@@ -0,0 +1,8 @@
+package com.shulga.taskplanner.interfaces;
+
+/**
+ * Interface for setting the date picker listener
+ */
+public interface IDatePickerDialogListener {
+    void setDate(int year, int month, int day);
+}
